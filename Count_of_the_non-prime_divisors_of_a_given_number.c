1d1c787837e199144bb25d5d5782b6742decd7be
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-04-22 21:36:16 +0530

    #include<stdio.h> int main() {     int n,i,fact=0,j,count=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         if(n%i==0)         {             fact=0;             for(j=1;j<=i;j++)             {                 if(i%j==0)                 {                     fact=fact+1;                 }             }             if(fact!=2)             {                 count+=1;             }         }     }     printf("%d",count);     return 0; }

--- Count_of_the_non-prime_divisors_of_a_given_number.c
+++ Count_of_the_non-prime_divisors_of_a_given_number.c
@@ -1,21 +1,26 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,c1=0,c2=0;
+    int n,i,fact=0,j,count=0;
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
         if(n%i==0)
         {
-            c2=0;
+            fact=0;
             for(j=1;j<=i;j++)
             {
                 if(i%j==0)
-                c2++;
+                {
+                    fact=fact+1;
+                }
             }
-            if(c2!=2)
-            c1++;
+            if(fact!=2)
+            {
+                count+=1;
+            }
         }
     }
-    printf("%d",c1);
+    printf("%d",count);
+    return 0;
 }
