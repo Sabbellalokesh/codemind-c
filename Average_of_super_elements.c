5f5ebb678ea4e25837d31a0486b01773a209c9ae
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-05-25 17:28:04 +0530

    #include<stdio.h> int main() {     int n,a[100],c=0,f=0;     float sum=0,avg;     scanf("%d",&n);     for(int i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(int i=0;i<n;i++)     { c=0;         for(int j=0;j<n;j++)         {             if(a[i]==a[j])             {                 c++;             }         }         if(c==a[i])         {             sum=sum+a[i];             f++;             a[i]=0;         }     }     if(f==0)     {         printf("-1");     }     else     {     avg=float(sum)/f;     printf("%.2f",avg);     } }

--- Average_of_super_elements.c
+++ Average_of_super_elements.c
@@ -1,15 +1,15 @@
 #include<stdio.h>
 int main()
 {
-    int n,a[100],k=0;
+    int n,a[100],c=0,f=0;
     float sum=0,avg;
     scanf("%d",&n);
-    for(int i=00;i<n;i++)
+    for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
     for(int i=0;i<n;i++)
-    {int c=0;
+    { c=0;
         for(int j=0;j<n;j++)
         {
             if(a[i]==a[j])
@@ -20,15 +20,17 @@ int main()
         if(c==a[i])
         {
             sum=sum+a[i];
+            f++;
             a[i]=0;
-            k++;
         }
     }
-    if(k==0)
-    printf("-1");
+    if(f==0)
+    {
+        printf("-1");
+    }
     else
     {
-        avg=float(sum)/k;
-        printf("%.2f",avg);
+    avg=float(sum)/f;
+    printf("%.2f",avg);
     }
 }
