0a2417fff3d2fe5d0b2a6640c35be0a8a5c7d292
lokesh reddy <97579926+Sabbellalokesh@users.noreply.github.com>
2022-07-07 21:35:32 +0530

    #include<stdio.h> int main() {     int n,m,a[100][100],b[100][100],c[100][100],j,s=0,s1=0;     scanf("%d %d",&n,&m);     for(int i=0;i<n;i++)     {         for(int j=0;j<m;j++)         {             scanf("%d",&a[i][j]);         }     }
        for(int j=0;j<m;j++)
        {  
            for(int i=0;i<n;i++)
            {
                if(i%2==0)
                s=s+a[i][j];
                else
                s1=s1+a[i][j];
            }
        }
        printf("%d %d",s,s1);
    }

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -1,26 +1,25 @@
 #include<stdio.h>
 int main()
 {
-    int a[100][100],n,m,sum=0,sum1=0;
-    scanf("%d%d",&n,&m);
+    int n,m,a[100][100],b[100][100],c[100][100],j,s=0,s1=0;
+    scanf("%d %d",&n,&m);
     for(int i=0;i<n;i++)
-    {
+    {  
         for(int j=0;j<m;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<m;j++)
+   
+    for(int j=0;j<m;j++)
+    {  
+        for(int i=0;i<n;i++)
         {
-          if(i%2==0)
-          sum=sum+a[i][j];
-          else
-          sum1+=a[i][j];
+            if(i%2==0)
+            s=s+a[i][j];
+            else
+            s1=s1+a[i][j];
         }
     }
-    
-    printf("%d ",sum);
-    printf("%d",sum1);
+    printf("%d %d",s,s1);
 }
